d7e72642b6e8a6ea513fc77633359e96404e71d0
PatoProgramador <skpates18@gmail.com>
2026-07-06 21:46:32 -0500

    Fix mongo run app

--- src/main/java/com/academianet/demo/DemoApplication.java
+++ src/main/java/com/academianet/demo/DemoApplication.java
@@ -2,8 +2,18 @@ package com.academianet.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(
+        basePackages = "com.academianet.demo.repository",
+        excludeFilters = @Filter(
+                type = FilterType.REGEX,
+                pattern = "com\\.academianet\\.demo\\.repository\\.mongo\\..*"))
+@EnableMongoRepositories(basePackages = "com.academianet.demo.repository.mongo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
